9234c54dbac65faac997d9fc3b55e43482d13a42
Steve Lee <stevelee527@gmail.com>
2016-03-28 18:58:03 -0400

    Database for Tournament Project
    Initial DB work up

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,11 +1,11 @@
  -- Table definitions for the tournament project.
 
--- Table to register players
-CREATE TABLE Players(id serial PRIMARY KEY, name varchar(200));
+-- Table to register Players
+CREATE TABLE Players(player_id serial, player_name varchar(200) PRIMARY KEY);
 
--- Table Collect Match Data
-CREATE TABLE Matches(id serial PRIMARY KEY, Players, game1 int, game2 int,
-    game3 int, game4 int);
+-- Table to Collect Match Data
+CREATE TABLE Matches(game_id serial PRIMARY KEY, player_id REFERENCES Players,
+    game1 int, game2 int, game3 int, game4 int);
 
 -- Table to Keep Player standings
-CREATE TABLE PlayerStandings()
+CREATE TABLE PlayerStandings(player_name REFERENCES Players, )
